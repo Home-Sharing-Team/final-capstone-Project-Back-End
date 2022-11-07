bd655f3686410740ce4bd46bf0b865a60ee49eaa
Unyielding1 <david.verde.alvarez@gmail.com>
2022-11-07 18:02:59 -0500

    create auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,16 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[index show create update destroy]
+      post 'auth/sign_in', to: 'authentication#sign_in'
+      post 'auth/sign_up', to: 'authentication#sign_up'
     end
   end
 end
+
+
+ #{
+  #success: true,
+  #data: {
+    #user: user,
+    #accessToken: JWT_TOKEN,
+  #}
