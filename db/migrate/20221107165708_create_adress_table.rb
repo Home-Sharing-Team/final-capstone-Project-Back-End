d3c50dbe5dcdd972782d78d2c6bde16d74913336
Unyielding1 <david.verde.alvarez@gmail.com>
2022-11-07 13:18:47 -0500

    Create tables after  migrate

--- db/migrate/20221107165708_create_adress_table.rb
+++ db/migrate/20221107165708_create_adress_table.rb
@@ -1,11 +1,11 @@
 class CreateAdressTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :adresses do |t|
-      t.string :street
-      t.string :number
-      t.string :city
-      t.string :country
-      t.string :zip_code
+    create_table :addresses do |t|
+      t.string :street , default: ""
+      t.integer :number , default: 0
+      t.string :city , default: ""
+      t.string :country , default: ""
+      t.string :zip_code , default: ""
 
       t.timestamps
     end
