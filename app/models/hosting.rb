d204cd2b6cbdc574b5fcfd012182af7735ad2515
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-11-07 18:01:05 -0400

    Add Hosting model

---
+++ app/models/hosting.rb
@@ -0,0 +1,11 @@
+class Hosting < ApplicationRecord
+  belongs_to :user
+  belongs_to :property
+  has_many :reservations
+
+  validates :cycle, presence: true
+  validates :minumum_cycle_amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :rate, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :cleaning_fee, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :public, presence: true
+end
