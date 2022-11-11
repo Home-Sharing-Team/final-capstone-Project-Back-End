0eb4310e5b30ab67d5bc970be2e15cc31444ff5b
Unyielding1 <david.verde.alvarez@gmail.com>
2022-11-11 13:43:30 -0500

    fix linters

--- spec/request/api/v1/hostings_spec.rb
+++ spec/request/api/v1/hostings_spec.rb
@@ -5,53 +5,50 @@ RSpec.describe 'hostings', type: :request do
     Rails.application.load_seed
   end
 
-    describe 'GET /api/v1/hostings' do
-        it 'returns all hostings' do
-        get '/api/v1/hostings'
+  describe 'GET /api/v1/hostings' do
+    it 'returns all hostings' do
+      get '/api/v1/hostings'
 
-        expect(response).to have_http_status(200)
+      expect(response).to have_http_status(200)
 
-        hostings = JSON.parse(response.body)
-        expect(hostings.length).to eq(2)
-        end
+      hostings = JSON.parse(response.body)
+      expect(hostings.length).to eq(2)
     end
+  end
 
-    describe 'POST /api/v1/hostings' do
-        subject { post '/api/v1/hostings', params: params }
-        context 'when the params are valid' do
-            let(:params) do
-                {
-                    cycle: 2, 
-                    minimum_cycle_amount: 2,
-                     rate: 12, 
-                     public: true, 
-                     cleaning_fee: 1, 
-                     user_id: 44, 
-                     property_id: 51
-                }
-            end
-           
-        end
-        context 'when the params are invalid' do
-            let(:params) { { name: '' } }
-            it 'returns unprocessable entity status' do
-                subject
-                expect(response).to have_http_status(401)
-            end
-        end
+  describe 'POST /api/v1/hostings' do
+    subject { post '/api/v1/hostings', params: }
+    context 'when the params are valid' do
+      let(:params) do
+        {
+          cycle: 2,
+          minimum_cycle_amount: 2,
+          rate: 12,
+          public: true,
+          cleaning_fee: 1,
+          user_id: 44,
+          property_id: 51
+        }
+      end
     end
-
-    describe 'GET /api/v1/hostings/:id' do
-        subject { get "/api/v1/hostings/#{hosting_id}" }
-
-        context 'when the hosting does not exist' do
-            let(:hosting_id) { 'invalid' }
-            it 'returns not found status' do
-                subject
-                expect(response).to have_http_status(404)
-            end
-        end
+    context 'when the params are invalid' do
+      let(:params) { { name: '' } }
+      it 'returns unprocessable entity status' do
+        subject
+        expect(response).to have_http_status(401)
+      end
     end
+  end
 
+  describe 'GET /api/v1/hostings/:id' do
+    subject { get "/api/v1/hostings/#{hosting_id}" }
 
+    context 'when the hosting does not exist' do
+      let(:hosting_id) { 'invalid' }
+      it 'returns not found status' do
+        subject
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
--- spec/request/api/v1/reservations_spec.rb
+++ spec/request/api/v1/reservations_spec.rb
@@ -1,36 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'reservations', type: :request do
-    before(:all) do
-        Rails.application.load_seed
+  before(:all) do
+    Rails.application.load_seed
+  end
+
+  describe 'GET /api/v1/reservations' do
+    it 'returns all reservations' do
+      get '/api/v1/reservations'
+
+      expect(response).to have_http_status(200)
+
+      reservations = JSON.parse(response.body)
+      expect(reservations.length).to eq(2)
     end
+  end
 
-    describe 'GET /api/v1/reservations' do
-        it 'returns all reservations' do
-        get '/api/v1/reservations'
+  describe 'GET /api/v1/reservations/:id' do
+    subject { get "/api/v1/reservations/#{reservation_id}" }
 
-        expect(response).to have_http_status(200)
-
-        reservations = JSON.parse(response.body)
-        expect(reservations.length).to eq(2)
-        end
+    context 'when the reservation does not exist' do
+      let(:reservation_id) { 'invalid' }
+      it 'returns not found status' do
+        subject
+        expect(response).to have_http_status(404)
+      end
     end
-
-   
-
-    describe 'GET /api/v1/reservations/:id' do
-        subject { get "/api/v1/reservations/#{reservation_id}" }
-
-        context 'when the reservation does not exist' do
-            let(:reservation_id) { 'invalid' }
-            it 'returns not found status' do
-                subject
-                expect(response).to have_http_status(404)
-            end
-        end
-    end
-
-
+  end
 end
-    
-      
