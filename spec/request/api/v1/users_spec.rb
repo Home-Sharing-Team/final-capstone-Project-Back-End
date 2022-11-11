942b038593d6b38f7d6b06dd665d0c2d4846e123
Unyielding1 <david.verde.alvarez@gmail.com>
2022-11-10 22:00:30 -0500

    fix linters

--- spec/request/api/v1/users_spec.rb
+++ spec/request/api/v1/users_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe 'users', type: :request do
     User.delete_all
   end
 
-
   describe 'GET /api/v1/users' do
     it 'returns all users' do
-      User.create!(email: "davidtest@gmail.com", name: "David", password: "123456")
+      User.create!(email: 'davidtest@gmail.com', name: 'David', password: '123456')
 
       get '/api/v1/users'
 
@@ -15,18 +14,6 @@ RSpec.describe 'users', type: :request do
 
       users = JSON.parse(response.body)
       expect(users.length).to eq(2)
-
     end
   end
-
-
-
-
 end
-
-
-
-
-
-
-
