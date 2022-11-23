96fe25d2755bc5619b501ec1e91040c3de80226b
Unyielding1 <david.verde.alvarez@gmail.com>
2022-11-23 18:06:44 -0500

    clean seeds using rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-
 # Create categories
 latests = Category.create(name: 'latests') # id = 1
 beachfront = Category.create(name: 'beachfront') # id = 2
@@ -18,7 +17,7 @@ beach = Category.create(name: 'beach') # 15
 castles = Category.create(name: 'castles') # 16
 
 # Create users
-admin = User.create(name: "Mr Admin", email: "admin@test.com", password: "123123", role: 1) # 1
+admin = User.create(name: 'Mr Admin', email: 'admin@test.com', password: '123123', role: 1) # 1
 david = User.create(name: 'David Verde', email: 'david@gmail.com', password: '123456') # 2
 gabriel = User.create(name: 'Gabriel Santo', email: 'gabriel@test.com', password: '123123') # 3
 Luke = User.create(name: 'Luke Stamford', email: 'luke@test.com', password: '123123') # 4
@@ -37,13 +36,13 @@ luxury_property = Property.create(
   guest_capacity: 9,
   bedrooms: 3,
   beds: 5,
-  baths: 3,  
+  baths: 3,
   kind: 'house',
   size: 690,
   address_id: 1,
   user_id: 3,
   is_public: true
-) 
+)
 
 PropertyImage.create(property_id: 1, source: 'https://i.ibb.co/XpgGTmY/62ad847e-2f84-49cb-a8a7-35856dc93a20.webp')
 PropertyImage.create(property_id: 1, source: 'https://i.ibb.co/103Qnyr/d88dfa7d-113a-4e26-b3fd-d819df85a446.webp')
@@ -60,49 +59,49 @@ PropertyCategory.create(property_id: 1, category_id: 9)
 PropertyCategory.create(property_id: 1, category_id: 15)
 
 Hosting.create(
-  cycle: 0, 
-  minimum_cycle_amount: 3, 
-  rate: 365, 
-  user_id: 3, 
+  cycle: 0,
+  minimum_cycle_amount: 3,
+  rate: 365,
+  user_id: 3,
   property_id: 1
-)  
+)
 Hosting.create(
-  cycle: 1, 
-  minimum_cycle_amount: 1, 
-  rate: 2415, 
-  user_id: 3, 
+  cycle: 1,
+  minimum_cycle_amount: 1,
+  rate: 2415,
+  user_id: 3,
   property_id: 1
 )
 
 BlockedPeriod.create(
-  start_date: '2022-11-20', 
-  end_date: '2022-11-25', 
-  property_id: 1 
+  start_date: '2022-11-20',
+  end_date: '2022-11-25',
+  property_id: 1
 )
 BlockedPeriod.create(
-  start_date: '2023-01-01', 
-  end_date: '2023-01-08', 
-  property_id: 1 
+  start_date: '2023-01-01',
+  end_date: '2023-01-08',
+  property_id: 1
 )
 
 # Create the Iconic Glass Mansion - Huge Views - Best Location Property
 glass_mansion_address = Address.create(street: 'Unknown', city: 'Osage Beach', country: 'USA', zip_code: '111112', number: 2, continent: 'North America') # id = 2
 
-glass_mansion = Property.create( 
+glass_mansion = Property.create(
   name: 'Iconic Glass Mansion - Huge Views - Best Location',
   description: 'The iconic and luxurious Glass Mansion has awe-inspiring, expansive lake views for miles from almost every room.\n It is located directly in the heart of Lake of the Ozarks in Osage Beach, yet is securely and privately tucked away in the prestigious gated Ledges Waterfront Homes neighborhood. Glass Mansion sits on the main channel of the lake directly on the water at the 20 mile marker, perched on beautiful natural rock cliffs with cascading waterfalls into the lake below.',
   guest_capacity: 16,
   bedrooms: 5,
   beds: 10,
   baths: 5,
-  type: 'house',
+  kind: 'house',
   size: 1200,
   is_public: true,
   address_id: 2,
   user_id: 3
 )
 
-PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/XpgGTmY/62ad847e-2f84-49cb-a8a7-35856dc93a20.webp')  
+PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/XpgGTmY/62ad847e-2f84-49cb-a8a7-35856dc93a20.webp')
 PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/103Qnyr/d88dfa7d-113a-4e26-b3fd-d819df85a446.webp')
 PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/N7rfLcs/c7ec5fd9-ae03-40f9-9632-9ce1a77a3df2.webp')
 PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/2j0Kvgy/6513c49a-7537-4175-be57-f7581f393b43.webp')
@@ -118,17 +117,17 @@ PropertyCategory.create(property_id: 2, category_id: 15)
 
 Hosting.create(
   cycle: 0,
-      minimum_cycle_amount: 3,
-      rate: 365,
-      user_id: 3,
-      property_id: 2
+  minimum_cycle_amount: 3,
+  rate: 365,
+  user_id: 3,
+  property_id: 2
 )
 Hosting.create(
   cycle: 0,
-        minimum_cycle_amount: 2,
-        rate: 370,
-        user_id: 3,
-        property_id: 2
+  minimum_cycle_amount: 2,
+  rate: 370,
+  user_id: 3,
+  property_id: 2
 )
 
 BlockedPeriod.create(
@@ -138,7 +137,7 @@ BlockedPeriod.create(
 )
 BlockedPeriod.create(
   start_date: '2023, 01, 01',
-  end_date: '2023, 01, 03',  
+  end_date: '2023, 01, 03',
   property_id: 2
 )
 BlockedPeriod.create(
@@ -154,15 +153,15 @@ six_palms_villas = Property.create(
   name: 'Luxury Beachfront Six Palms Villas # 1',
   description:
   'This is the house perfect for you! Near to the beach, to markets, gyms, etc. Reserve a date!',
-guest_capacity: 9,
-bedrooms: 3,
-beds: 5,
-baths: 3,
-type: 'house',
-size: 690,
-is_public: true,
-address_id: 3,
-user_id: 3
+  guest_capacity: 9,
+  bedrooms: 3,
+  beds: 5,
+  baths: 3,
+  kind: 'house',
+  size: 690,
+  is_public: true,
+  address_id: 3,
+  user_id: 3
 )
 
 PropertyImage.create(property_id: 3, source: 'https://i.ibb.co/XpgGTmY/62ad847e-2f84-49cb-a8a7-35856dc93a20.webp')
@@ -213,14 +212,14 @@ glass_mansion = Property.create(
   bedrooms: 5,
   beds: 10,
   baths: 5,
-  type: 'house',
+  kind: 'house',
   size: 1200,
   is_public: true,
   address_id: 4,
   user_id: 4
 )
 
-PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/N35JMr8/abfdccee-b6d6-4465-89e9-6625482abeb7.webp')  
+PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/N35JMr8/abfdccee-b6d6-4465-89e9-6625482abeb7.webp')
 PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/kxmf2cb/db6f128c-e396-4835-944d-ce71b66e7dc0.webp')
 PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/mvtfrWB/99038ba2-d090-4533-b234-607d20d26000.webp')
 PropertyImage.create(property_id: 2, source: 'https://i.ibb.co/4FbXqYK/693f98b2-4635-466f-becf-d84cd41150bd.webp')
@@ -243,7 +242,7 @@ Hosting.create(
 Hosting.create(
   cycle: 2,
   minimum_cycle_amount: 1,
-  rate: 10000,
+  rate: 10_000,
   user_id: 4,
   property_id: 4
 )
@@ -271,16 +270,16 @@ beachfront_address = Address.create(street: 'Unknown', city: 'Malibu', country: 
 
 beachfront = Property.create(
   name: 'Beachfront Malibu Retreat',
-  description:
+  description: 'This is the house perfect for you! Near to the beach, to markets, gyms, etc. Reserve a date!',
   guest_capacity: 2,
-    bedrooms: 1,
-    beds: 1,
-    baths: 1,
-    type: 'apartment',
-    size: 60,
-    is_public: true,
-    address_id: 5,
-    user_id: 5
+  bedrooms: 1,
+  beds: 1,
+  baths: 1,
+  kind: 'apartment',
+  size: 60,
+  is_public: true,
+  address_id: 5,
+  user_id: 5
 )
 
 PropertyImage.create(property_id: 5, source: 'https://i.ibb.co/P98xkTQ/56fe9e3f-48c1-4777-a2b0-5b7745eb9874.webp')
@@ -336,14 +335,14 @@ songbird = Property.create(
   name: 'Songbird Studio- Secluded but close to everything!',
   description: 'Bright, sunny, secluded, green 600 sqft studio apartment at rear of Antique home in the heart of Brewster, Northside of Rt 6A. Private entrance. Private backyard with lawn & gardens. Small private deck, gas grill. Bird and small animal life abounds outside. Tons of stars at night.. salt air on the wind. Walking distance to coffee shop, convenience store, multiple restaurants, Bike Trail. Short ride to bay beaches, easy drive to Ocean beaches. Gas "woodstove" for heat, AC for cooling, be comfy!',
   guest_capacity: 4,
-    bedrooms: 1,
-    beds: 2,
-    baths: 1,
-    type: 'house',
-    size: 80,
-    is_public: true,
-    address_id: 6,
-    user_id: 6
+  bedrooms: 1,
+  beds: 2,
+  baths: 1,
+  kind: 'house',
+  size: 80,
+  is_public: true,
+  address_id: 6,
+  user_id: 6
 )
 
 PropertyImage.create(property_id: 6, source: 'https://i.ibb.co/3kPZW0f/3c0dfe9f-6df6-467a-9c80-4edbfe5dc61d.webp')
@@ -394,15 +393,15 @@ provenza = Property.create(
   name: 'Provenza Life 1002- Gorgeus apartment/gym',
   description:
       'This special place is close to everything, making it easy to plan your visit and move around while enjoying your trip.\n Only has 1 AC in the main room',
-    guest_capacity: 2,
-    bedrooms: 1,
-    beds: 1,
-    baths: 2,
-    type: 'apartment',
-    size: 55,
-    is_public: true,
-    address_id: 7,
-    user_id: 7
+  guest_capacity: 2,
+  bedrooms: 1,
+  beds: 1,
+  baths: 2,
+  kind: 'apartment',
+  size: 55,
+  is_public: true,
+  address_id: 7,
+  user_id: 7
 )
 
 PropertyImage.create(property_id: 7, source: 'https://i.ibb.co/y4hsHMP/0451b654-545c-40e9-9416-458399129abf.webp')
@@ -459,15 +458,15 @@ grand_castle = Property.create(
   name: '2BR Suite @ The Grand Castle | Pool/Gym/Parking',
   description:
       'Featuring authentic architecture, high-end finishes, and attractive amenities, The Grand Castle is a unique experience for the entire family. Designed after the Neuschwanstein Castle in southwest Bavaria, the 15 story Grand Castle is the 2nd largest castle structure in the world. The kids will love the outdoor heated pool, game room, and Beauty and the Beast replicate library. Located just 30 minutes from Lake Michigan and 10 minutes from downtown Grand Rapids, there is plenty for the adults too',
-    guest_capacity: 6,
-    bedrooms: 2,
-    beds: 2,
-    baths: 2,
-    type: 'apartment',
-    size: 80,
-    is_public: true,
-    address_id: 8,
-    user_id: 8
+  guest_capacity: 6,
+  bedrooms: 2,
+  beds: 2,
+  baths: 2,
+  kind: 'apartment',
+  size: 80,
+  is_public: true,
+  address_id: 8,
+  user_id: 8
 )
 
 PropertyImage.create(property_id: 8, source: 'https://i.ibb.co/WFMLk4H/595b5f29-3b46-4441-aa67-a2be6939c84e.webp')
@@ -531,15 +530,15 @@ chateau_pessac = Property.create(
   name: 'Chateau Pessac',
   description:
       'When the turreted roof of this chateau comes into view, guests will begin to understand the scale of grandeur this home represents. Painstakingly preserved and refitted to satisfy the most expectant of modern guests, this centuries-old chateau offers room after room of history and plush comfort with every step, right on the doorstep of metropolitan Bordeaux and France’s finest wine country.',
-    guest_capacity: 12,
-    bedrooms: 6,
-    beds: 6,
-    baths: 7,
-    type: 'house',
-    size: 2200,
-    is_public: true,
-    address_id: 9,
-    user_id: 9
+  guest_capacity: 12,
+  bedrooms: 6,
+  beds: 6,
+  baths: 7,
+  kind: 'house',
+  size: 2200,
+  is_public: true,
+  address_id: 9,
+  user_id: 9
 )
 
 PropertyImage.create(property_id: 9, source: 'https://i.ibb.co/7rfyBrH/5cf60fda-63c1-45d3-84ad-e5c6f593f4af.webp')
@@ -580,20 +579,6 @@ BlockedPeriod.create(
   end_date: '2023, 01, 25',
   property_id: 9
 )
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
 
 # Address.create(street: 'Calle 1', city: 'Bogota', country: 'Colombia', zip_code: '111111', number: 1)
 # Address.create(street: 'Calle 2', city: 'Taiwan', country: 'China', zip_code: '111111', number: 2)
