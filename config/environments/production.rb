894c848fdd785fe3309ac56b61339d17cfa28536
Gabriel Santo <gvgesanto2@gmail.com>
2022-12-12 16:27:40 -0300

    feat: Add hostname to the production and development enviroment configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "home-sharing-api.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Code is not reloaded between requests.
